dd5e6d096f8a5e99cb3ecb7e613eda962ae9e6d9
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-12-11 12:53:12 -0300

    :recycle: Moved the user profile importing to a function, to make things mantainable

--- init.lua
+++ init.lua
@@ -1,13 +1,18 @@
-local NEOVIM_CONFIG_PATH = os.getenv("HOME").."/.config/nvim"
-local SHARED_CONFIG_PATH = NEOVIM_CONFIG_PATH.."/shared"
-local SHARED_CONFIG_FILES = {
-    "keymaps.vim",
-    "commands.vim",
-    "indentation_rules.vim",
-    "settings.vim",
-}
+local function load_user_profile()
+    local NEOVIM_CONFIG_PATH = os.getenv("HOME").."/.config/nvim"
+    local SHARED_CONFIG_PATH = NEOVIM_CONFIG_PATH.."/shared"
 
-for _, file in ipairs(SHARED_CONFIG_FILES) do
-    local vim_command = "source "..SHARED_CONFIG_PATH.."/"..file
-    vim.cmd(vim_command)
+    local SHARED_CONFIG_FILES = {
+        "keymaps.vim",
+        "commands.vim",
+        "indentation_rules.vim",
+        "settings.vim",
+    }
+    
+    for _, file in ipairs(SHARED_CONFIG_FILES) do
+        local vim_command = "source "..SHARED_CONFIG_PATH.."/"..file
+        vim.cmd(vim_command)
+    end
 end
+
+load_user_profile()
