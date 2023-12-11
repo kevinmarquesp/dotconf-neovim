85a8da6f0459c31b6fc437c18f30b96368de339c
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-12-11 19:13:48 -0300

    :sparkles: Lualine plugin added and configured

--- lua/plug_list.lua
+++ lua/plug_list.lua
@@ -9,5 +9,6 @@ table.insert(plug_list, require("user_plugins.c_treesitter").get())
 table.insert(plug_list, require("user_plugins.p_telescope").get())
 table.insert(plug_list, require("user_plugins.p_cmp").get())
 table.insert(plug_list, require("user_plugins.c_lspconfig").get())
+table.insert(plug_list, require("user_plugins.p_lualine").get())
 
 return plug_list
