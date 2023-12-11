309e19bb8b57af3ffcb2bf8ce5dc44cf45d37e83
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-12-11 16:38:21 -0300

    :sparkles: Emmet plugin added and configured

--- lua/plug_list.lua
+++ lua/plug_list.lua
@@ -4,4 +4,6 @@ table.insert(plug_list, "kyazdani42/nvim-web-devicons")
 table.insert(plug_list, "tpope/vim-commentary")
 table.insert(plug_list, "tpope/vim-surround")
 
+table.insert(plug_list, require("user_plugins.p_emmet").get())
+
 return plug_list
