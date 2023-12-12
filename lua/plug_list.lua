3aeace69c220dcef2d6e6984a3fcdacecb6e65af
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-12-12 13:52:12 -0300

    :sparkles: (plug) Reconfigured Emmet plugin config file

--- lua/plug_list.lua
+++ lua/plug_list.lua
@@ -3,12 +3,6 @@ local plug_list = {}
 table.insert(plug_list, "kyazdani42/nvim-web-devicons")
 table.insert(plug_list, "tpope/vim-surround")
 
-table.insert(plug_list, require("user_plugins.p_nord").get())
-table.insert(plug_list, require("user_plugins.p_emmet").get())
-table.insert(plug_list, require("user_plugins.c_treesitter").get())
-table.insert(plug_list, require("user_plugins.p_telescope").get())
-table.insert(plug_list, require("user_plugins.p_cmp").get())
-table.insert(plug_list, require("user_plugins.c_lspconfig").get())
-table.insert(plug_list, require("user_plugins.p_lualine").get())
+table.insert(plug_list, require('plugins.p_emmet'))
 
 return plug_list
