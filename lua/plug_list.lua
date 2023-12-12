175d7d07de67ef44cf6a506fc565fc6173b5cf36
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-12-12 14:02:16 -0300

    :sparkles: (plug) Created a plugin group that is related to the visual style of my editor

--- lua/plug_list.lua
+++ lua/plug_list.lua
@@ -4,5 +4,6 @@ table.insert(plug_list, "kyazdani42/nvim-web-devicons")
 table.insert(plug_list, "tpope/vim-surround")
 
 table.insert(plug_list, require('plugins.p_emmet'))
+table.insert(plug_list, require('plugins.c_editor_style'))
 
 return plug_list
