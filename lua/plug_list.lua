22c2702947f31122cd39f2b54d54f63a1617fc25
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-01-12 18:32:57 -0300

    :fire: removed the emmet.vim plugin from my enviroment

--- lua/plug_list.lua
+++ lua/plug_list.lua
@@ -3,7 +3,6 @@ local plug_list = {}
 table.insert(plug_list, "kyazdani42/nvim-web-devicons")
 table.insert(plug_list, "tpope/vim-surround")
 
-table.insert(plug_list, require('plugins.p_emmet'))
 table.insert(plug_list, require('plugins.c_editor_style'))
 table.insert(plug_list, require('plugins.c_editor_interface'))
 table.insert(plug_list, require('plugins.p_telescope'))
